4ce43e250db0246bf84edaa4eb24502a2ced457d
sbgarcia <sharonbondoc.g@gmail.com>
2020-09-15 08:58:54 -0700

    connected mongoose mongodb atlas

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
+// require statements
 const express = require('express')
 const app = express()
-const port = 5000
 
+// for mongodb atlas - needs to be right under express require statements
+require("dotenv").config()
 
-app.get('/', (request, response) => {
-    response.send("hello world")
-})
+const mongoose = require('mongoose')
 
+const port = process.env.PORT || 5000
+
+// mongoose mongodb connections
+const connectionString = process.env.MONGODB_URI || "mongodb://localhost:27017/corgi"
+
+const configOptions = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}
+
+mongoose.connect(connectionString, configOptions)
+    .then(() => console.log("MongoDB successfully connected", connectionString))
+    .catch(err => console.log(`MongoDB connection error: ${err}`))
+
+// connection
 app.listen(port, () => console.log(`Server is running on port ${port}`))
