934d27b956652ac7a83cbc76672eb4c807491a1b
sbgarcia <sharonbondoc.g@gmail.com>
2020-09-16 22:08:44 -0700

    fixed router variable in chat controller

--- controllers/chat.js
+++ controllers/chat.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (request, response) => {
-    response.send("Server is up and running")
+    response.send("hello world")
 })
 
 module.exports = router
